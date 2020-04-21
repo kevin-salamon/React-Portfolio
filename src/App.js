64b663ed1ff5cc1184bb486192e773795a597a8e
kevin-salamon <kevinwebdevs@gmail.com>
2020-04-21 09:40:02 -0400

    added construction outline for skills section on main page

--- src/App.js
+++ src/App.js
@@ -11,33 +11,76 @@ function App() {
     </div>
 
     <div className="container rounded" style={{backgroundColor: "white", color: "black"}}>
-    <div className="row">
-      <div className="col-sm-12 text-center intro-index">
-        An up-and-coming Full-Stack Web Developer
+      <div className="row">
+        <div className="col-sm-12 text-center intro-index">
+          An up-and-coming Full-Stack Web Developer
+        </div>
+      </div>
+      <div className="row mb-5">
+        <div className="col-sm-4 center-small">
+          <p className="mt-5 sub-intro-index">WHO AM I?</p>
+          <p className="content-text">Hi there - I'm a full-stack web developer who specializes in Javascript, and has a background in React, NodeJS, MongoDB, Bootstrap, and more.
+            I've studied political science and worked for political campaigns, have experience in law offices, and even did a stint as a pool boy. Now, I'm working to develop
+            streamlined, functional, and modern web applications. Please see below for my full resume, and feel free to peruse my portfolio or reach out to me for more info.
+          </p>
+        </div>
+        <div className="col-sm-4 text-right">
+          <img src={require("./assets/images/profile.jpg")} className="align-self-center mt-5 mb-1 mr-2 ml-2 rounded-pill image-large hide-small" alt={"profile"} />
+        </div>
+        <div className="col-sm-4 center-small" style={{paddingLeft:"5%", marginBottom:"30px"}}>
+          <p className="mt-5 sub-intro-index">MY INFO</p>
+          <p className="sub-sub-intro-index">Name</p>
+          <p className="content-text">Kevin Salamon</p>
+          <p className="sub-sub-intro-index">Location</p>
+          <p className="content-text">Long Valley, NJ, USA</p>
+          <p className="sub-sub-intro-index">How can I best reach you?</p>
+          <p className="content-text">Reach out to me by e-mail at <u>theksalamon@gmail.com</u></p>
+        </div>
       </div>
     </div>
-    <div className="row mb-5">
-      <div className="col-sm-4 center-small">
-        <p className="mt-5 sub-intro-index">WHO AM I?</p>
-        <p className="content-text">Hi there - I'm a full-stack web developer who specializes in Javascript, and has a background in React, NodeJS, MongoDB, Bootstrap, and more.
-          I've studied political science and worked for political campaigns, have experience in law offices, and even did a stint as a pool boy. Now, I'm working to develop
-          streamlined, functional, and modern web applications. Please see below for my full resume, and feel free to peruse my portfolio or reach out to me for more info.
-        </p>
+    <div className="skills-container">
+      <h1 className="skill-area-title">Tools</h1>
+      <div className="skills-section-flex">
+        <div className="skill-box-title">
+          <h1>Frontend</h1>
+        </div>
+        <div className="skill-box">
+          <h1>React.js</h1>
+          <h1>CSS/Bootstrap</h1>
+          <h1>Handlebars.js</h1>
+        </div>
+        <div className="skill-box">
+          <h1>JQuery</h1>
+          <h1>CSS/Bootstrap</h1>
+          <h1>Handlebars.js</h1>
+        </div>
+        <div className="skill-box">
+          <h1>React.js</h1>
+          <h1>CSS/Bootstrap</h1>
+          <h1>Handlebars.js</h1>
+        </div>
       </div>
-      <div className="col-sm-4 text-right">
-        <img src={require("./assets/images/profile.jpg")} className="align-self-center mt-5 mb-1 mr-2 ml-2 rounded-pill image-large hide-small" alt={"profile"} />
-      </div>
-      <div className="col-sm-4 center-small" style={{paddingLeft:"5%", marginBottom:"30px"}}>
-        <p className="mt-5 sub-intro-index">MY INFO</p>
-        <p className="sub-sub-intro-index">Name</p>
-        <p className="content-text">Kevin Salamon</p>
-        <p className="sub-sub-intro-index">Location</p>
-        <p className="content-text">Long Valley, NJ, USA</p>
-        <p className="sub-sub-intro-index">How can I best reach you?</p>
-        <p className="content-text">Reach out to me by e-mail at <u>theksalamon@gmail.com</u></p>
+      <div className="skills-section-flex">
+        <div className="skill-box-title">
+          <h1>Backend</h1>
+        </div>
+        <div className="skill-box">
+          <h1>React.js</h1>
+          <h1>CSS/Bootstrap</h1>
+          <h1>Handlebars.js</h1>
+        </div>
+        <div className="skill-box">
+          <h1>JQuery</h1>
+          <h1>CSS/Bootstrap</h1>
+          <h1>Handlebars.js</h1>
+        </div>
+        <div className="skill-box">
+          <h1>React.js</h1>
+          <h1>CSS/Bootstrap</h1>
+          <h1>Handlebars.js</h1>
+        </div>
       </div>
     </div>
-  </div> 
   <Footer />
   </>
   );
