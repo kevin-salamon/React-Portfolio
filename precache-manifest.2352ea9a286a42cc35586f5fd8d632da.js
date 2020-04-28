3348fe940a291aa2516c8c4b12ccb41288666806
kevin-salamon <kevinwebdevs@gmail.com>
2020-04-28 12:27:40 -0400

    Updates

---
+++ precache-manifest.2352ea9a286a42cc35586f5fd8d632da.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "652be495596b6495904d8a4cd059121c",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "428efc11483db1deb1ec",
+    "url": "/React-Portfolio/static/css/main.51a04a39.chunk.css"
+  },
+  {
+    "revision": "2a731893a1822c6428b1",
+    "url": "/React-Portfolio/static/js/2.aaeaf343.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Portfolio/static/js/2.aaeaf343.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "428efc11483db1deb1ec",
+    "url": "/React-Portfolio/static/js/main.ced116e7.chunk.js"
+  },
+  {
+    "revision": "b1cad2d636aa2549d2e5",
+    "url": "/React-Portfolio/static/js/runtime-main.bf13817d.js"
+  },
+  {
+    "revision": "7e15502d687e354a3b73b69d401a3562",
+    "url": "/React-Portfolio/static/media/gamefound.7e15502d.jpg"
+  },
+  {
+    "revision": "73befd1b0be534e5c35c74ce6b1fa0b4",
+    "url": "/React-Portfolio/static/media/momsite.73befd1b.png"
+  },
+  {
+    "revision": "50e9d86aaca0bf20d7c9a2ed2355f1b6",
+    "url": "/React-Portfolio/static/media/prof.50e9d86a.jpg"
+  },
+  {
+    "revision": "53fb0ac322c7a73e3be14ee8e3836356",
+    "url": "/React-Portfolio/static/media/prof2.53fb0ac3.jpg"
+  },
+  {
+    "revision": "a615ed4c2e5ea08241de000a4a63f80f",
+    "url": "/React-Portfolio/static/media/profile.a615ed4c.jpg"
+  },
+  {
+    "revision": "fd93b940ec26a3b8af47d82ba3dbd326",
+    "url": "/React-Portfolio/static/media/tablify.fd93b940.png"
+  }
+]);
